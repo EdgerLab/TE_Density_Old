628d514706bf27f06c2cf91ec8d43e0edc8e772d
huckleberry-hound <teresiscott@gmail.com>
2018-06-27 15:26:44 +0000

    First addition count calculator

--- Expression/expression.py
+++ Expression/expression.py
@@ -1,18 +1,16 @@
 # By Scott Teresi
-# The below script calculates TPM for a strawberry
-# The code first has to create the data structure for the genes
+# The below script creates a count file in preparation for RPKM, FPKM, and TPM
 #==================================================================
 import sys, re, os, time, csv
 from multiprocessing import Process
 from collections import deque
-#sys.path.insert(0, '/home/scott/Documents/Uni/Research/transposon_2.0/Camarosa_TE/') # specify path for import
-#import genic_elements
-genes = deque() # use a deque structure for containing my elements
-count_list = ['ERR855501.htseq.count','ERR855502.htseq.count','ERR855503.htseq.count','ERR855504.htseq.count','ERR855505.htseq.count','ERR855506.htseq.count']
-count_dict = {'ERR855501.htseq.count':'count_0','ERR855502.htseq.count':'count_1','ERR855503.htseq.count':'count_2','ERR855504.htseq.count':'count_3','ERR855505.htseq.count':'count_4','ERR855506.htseq.count':'count_5'}
-gene_dictionary = {}
 
-#count_dict = {'ERR855501.htseq.count':'count_0','ERR855502.htseq.count':'count_1'}#,'ERR855503.htseq.count':'count_2','ERR855504.htseq.count':'count_3','ERR855505.htseq.count':'count_4','ERR855506.htseq.count':'count_5'}
+genes = deque() # use a deque structure for containing my elements
+count_dict = {'ERR855501.htseq.count':'count_0','ERR855502.htseq.count':'count_1','ERR855503.htseq.count':'count_2','ERR855504.htseq.count':'count_3','ERR855505.htseq.count':'count_4','ERR855506.htseq.count':'count_5'}
+
+# Specify your files here!!! Especially your count file and it's associated attribute
+
+gene_dictionary = {}
 count_output = 'expression_out.csv'
 #==================================================================
 
@@ -80,10 +78,6 @@ def total_count():
         for elem in genes:
             setattr(elem,specific_attribute,total)
 
-
-
-
-
 def TPM(total_count):
     for elem in genes:
         length = elem.length/1000 # we want it in kb
@@ -92,61 +86,16 @@ def TPM(total_count):
             count_over_length = count/length
             setattr(elem,x,count)
 
-
-
-
-
-    #for elem in genes:
-        #for x in ['start', 'stop']:
-            #print(getattr(elem, x))
-            #print()
-
-
-
-    #for elem in genes:
-        #print(elem.__dict__)
-
-
-        #b = [a for a in dir(elem) if not a.startswith('__')]
-        #print(b)
-        #for item in b:
-            #if item[0:5] == 'count':
-                #getattr(
-                #item += 5
-
-
-def write_test():
-    for x in ['count_0','count_1','count_2','count_3','count_4','count_5']:
-        specific_attribute = 'total_'+x
-        print(specific_attribute)
-
-def count_sum():
-    with open('ERR855501.htseq.count') as f_in:
-        total = 0
-        for row in f_in:
-            row = re.split('\t+',row)
-            count = int(row[1])
-            total += count
-    print(total)
-
 #=============================================================
 def write_structure(a_dictionary):
     with open(count_output, 'w') as f_out:
-        fieldnames = ['name', 'chromosome', 'start', 'stop', 'length','count_0','count_1','count_2','count_3','count_4','count_5', 'total_count_0','total_count_1','total_count_2','total_count_3','total_count_4','total_count_5']
+        fieldnames = ['name', 'chromosome', 'start', 'stop', 'length','count_0','count_1','count_2','count_3','count_4','count_5', 'total_count_0','total_count_1','total_count_2','total_count_3','total_count_4','total_count_5'] # make sure your fieldnames contains strings of all your attributes
         f_out = csv.DictWriter(f_out,fieldnames=fieldnames)
         f_out.writeheader()
         for elem in a_dictionary:
             f_out.writerow(elem.__dict__)
 
-def info():
-    print('module name:', __name__)
-    print('Started algorithm')
-    print('parent process:', os.getppid())
-    print('process id:', os.getpid())
-    print()
-
 def run_all(gtf_inputfile,mRNA_inputfile):
-    info()
     gene_handler(mRNA_inputfile)
     gene_handler_2(gtf_inputfile)
     count_iterator(count_dict)
@@ -160,9 +109,10 @@ class Gene(object):
         self.chromosome = chromosome
         self.start = int(start)
         self.stop = int(stop)
-        gene_dictionary[maker_name]=self
+        gene_dictionary[maker_name] = self # add the object reference into the dictionary
 
-        #count_list = ['ERR855501.htseq.count','ERR855502.htseq.count','ERR855503.htseq.count','ERR855504.htseq.count','ERR855505.htseq.count','ERR855506.htseq.count']
+        # Make sure you get the right number of count and total count attributes made. I had 0-5
+
         self.count_0 = 0
         self.count_1 = 0
         self.count_2 = 0
@@ -194,24 +144,4 @@ class Gene(object):
 
 #===============================================================
 if __name__ == '__main__':
-    count_sum()
     run_all('camarosa_gtf_data.gtf','mRNA.bed')
-    #write_test()
-    #run_all('xx00','mRNA00')
-
-
-
-    #my_inputs = [['xx00','mRNA00']]#,['xx01','mRNA01'],['xx02','mRNA02'],['xx03','mRNA03'],['xx04','mRNA04'],['xx05','mRNA05'],
-                #['xx06','mRNA06']]
-    #p_list = []
-    #for f_name in my_inputs:
-        #p = Process(target=run_all, args=(f_name[0],f_name[1],))
-        #p.start()
-        #p_list.append(p)
-
-    #for p in p_list:
-        #current_pid = p.pid
-        #p.join(None)
-        #print("pid '{}' joined!".format(current_pid))
-
-
