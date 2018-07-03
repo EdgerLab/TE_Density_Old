579cb20ce2127efe39755df2e7b8824f9bcc5465
huckleberry-hound <teresiscott@gmail.com>
2018-07-03 09:50:15 +0000

    First addition of GO code

---
+++ GO/DensityGrouping.py
@@ -0,0 +1,8 @@
+# By Scott Teresi --- July 2018
+#---------------------------------------------
+import csv
+
+# This function groups the TE's of my file into bins for GO enrichment.
+# I use this file to partition the density of my genes
+
+
